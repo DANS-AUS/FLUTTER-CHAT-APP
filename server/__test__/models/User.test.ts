ac71909eb0769eeb336adc9daf5ace8a4a334c6e
Aaron Sanchez <saarong12@gmail.com>
2024-09-16 12:42:25 -0500

    Added test to validate that a user can add friends.

--- server/__test__/models/User.test.ts
+++ server/__test__/models/User.test.ts
@@ -1,4 +1,4 @@
-import { HydratedDocument } from 'mongoose'
+import { HydratedDocument, Types } from 'mongoose'
 import { serverConnect, serverDisconnect } from '../../db/testConfig'
 import { IUser } from '../../src/interfaces'
 import { User } from '../../src/models'
@@ -12,15 +12,61 @@ afterAll(async () => {
 })
 
 describe('User model', () => {
+  const userOneID = new Types.ObjectId()
+  const userTwoID = new Types.ObjectId()
+
+  const testUserOne: IUser = {
+    authId: 'a1b2c3',
+    username: 'testUserOne'
+  }
+
+  const testUserTwo: IUser = {
+    authId: 'd4e5f6',
+    username: 'testUserTwo'
+  }
   test('should create a user', async () => {
-    const testUser: IUser = {
-      authId: 'a1b2c3',
-      username: 'testUser'
-    }
+    const [newUserOne, newUserTwo]: HydratedDocument<IUser>[] =
+      await User.create([
+        { _id: userOneID, ...testUserOne },
+        { _id: userTwoID, ...testUserTwo }
+      ])
 
-    const newUser = await User.create(testUser)
+    expect(newUserOne).toBeInstanceOf(User)
+    expect(newUserOne).toMatchObject(testUserOne)
 
-    expect(newUser).toBeInstanceOf(User)
-    expect(newUser).toMatchObject(testUser)
+    expect(newUserTwo).toBeInstanceOf(User)
+    expect(newUserTwo).toMatchObject(testUserTwo)
+  })
+
+  test('should be able to have friends', async () => {
+    // TODO: Create a helper function for this in the API
+    await User.findByIdAndUpdate(userOneID, {
+      $push: { friends: userTwoID }
+    })
+    await User.findByIdAndUpdate(userTwoID, {
+      $push: { friends: userOneID }
+    })
+
+    // Find the user with the updated friend object id but no addition
+    // user info for the assertions.
+    const userOneWithFriendId = await User.findById(userOneID)
+    const userTwoWithFriendId = await User.findById(userTwoID)
+
+    const userOneWithFriendDetails = await User.findById(userOneID).populate(
+      'friends'
+    )
+    const userTwoWithFriendDetails = await User.findById(userTwoID).populate(
+      'friends'
+    )
+
+    expect(userOneWithFriendDetails?.friends?.length).toBe(1)
+    expect(userOneWithFriendDetails?.friends).toMatchObject([
+      userTwoWithFriendId
+    ])
+
+    expect(userTwoWithFriendDetails?.friends?.length).toBe(1)
+    expect(userTwoWithFriendDetails?.friends).toMatchObject([
+      userOneWithFriendId
+    ])
   })
 })
