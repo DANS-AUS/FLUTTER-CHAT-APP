328881dc0d167c4dcabdde5a25213427d18e7c64
DominiqueNix <dominiquenix@google.com>
2024-09-18 09:34:09 -0500

    created update a user route

--- server/__test__/routes/userRoutes.test.ts
+++ server/__test__/routes/userRoutes.test.ts
@@ -26,13 +26,35 @@ afterAll(async () => {
 
 describe('User Routes', () => {
     test('GET /:id should return a user based on the auth0 id',async () => {
-        const user1 = {authId: "1234", username: "user1"}
+        const user1: IUser = {authId: "1234", username: "user1"}
 
         const user : HydratedDocument<IUser> = await User.create(user1);
        
-        const res = await request(app).get('/api/v1/users/1234').send();
+        const res = await request(app).get('/api/v1/users/1234');
 
         expect(res.status).toBe(200);
         expect(res.body.user.username).toEqual(user1.username);
+    })
+
+    test('POST / should create a new user', async () => {
+        const user : IUser = { authId: "567", username: 'userName' } 
+        const res = await request(app).post('/api/v1/users').send({ ...user });
+        const userCheck : HydratedDocument<IUser> | null = await User.findOne({authId: user.authId});
+
+        expect(res.status).toBe(201);
+        expect(userCheck?.username).toEqual(user.username);
+    })
+
+    test('PUT /:id should update a user', async () => {
+        const oldUser : IUser = { authId: "123", username: 'oldUserName' };
+        await User.create(oldUser);
+
+        const newUserInfo : IUser = { authId: "123", username: 'newUserName' }; 
+        const res = await request(app).put('/api/v1/users/123').send({ ...newUserInfo });
+        
+        const userCheck : HydratedDocument<IUser> | null = await User.findOne({authId: oldUser.authId});
+
+        expect(res.status).toBe(200);
+        expect(userCheck?.username).toEqual(newUserInfo.username);
     })
 })
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -21,4 +21,27 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) : Pro
         next(err)
     }
 })
+
+// POST a new user. This route will be called by auth0 actions to create a user upon successful auth0 registration
+router.post("/", async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
+    try{
+        const { authId, username } = req.body;
+        await User.create({authId: authId, username: username});
+
+        res.status(201).send('New user created');
+    }catch(err){
+        next(err)
+    }
+})
+
+// PUT (update) a user
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) : Promise<void> =>{
+    try{
+        const { id } = req.params;
+        await User.findOneAndUpdate({authId: id}, req.body);
+        res.send(200)
+    }catch(err){
+        next(err)
+    }
+})
 export default router;
