f7aaf3e043ff63144131a253bb40b5808afbccf2
Aaron Sanchez <saarong12@gmail.com>
2024-09-17 23:49:26 -0500

    Added todo comments where I left off.

--- server/src/interfaces/IMessage.ts
+++ server/src/interfaces/IMessage.ts
@@ -1,5 +1,6 @@
 import { Types } from 'mongoose'
 
+// TODO: Edit the timestamp?
 export interface IMessage {
   sender: Types.ObjectId
   message: string
--- server/src/interfaces/IUser.ts
+++ server/src/interfaces/IUser.ts
@@ -1,5 +1,13 @@
 import { Types } from 'mongoose'
 
+// TODO: Edit the following fields:
+// [ ] Remove:
+// [ ] firstname
+// [ ] lastname
+// [ ] Add:
+// [ ] pendingFriends: Types.ObjectId
+// [ ] notifications: Types.ObjectId
+// [ ] newUser: Bool (default true)
 export interface IUser {
   authId: string
   username?: string
