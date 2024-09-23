7544a66f2f88a5fcc375a0f8263f121273e1f444
Aaron Sanchez <saarong12@gmail.com>
2024-09-23 12:12:57 -0500

    Created test to validate GET chat by id.

--- server/__test__/routes/chatRoutes.test.ts
+++ server/__test__/routes/chatRoutes.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest'
+import { NextFunction, Request, response, Response } from 'express'
+import { Types } from 'mongoose'
 import app from '../../src/app'
-import { NextFunction, Request, Response } from 'express'
+import { serverConnect, serverDisconnect } from '../../db/testConfig'
+import { Chat, Message, User } from '../../src/models'
 
 jest.mock('express-oauth2-jwt-bearer', () => ({
   auth: jest.fn(() => {
@@ -10,11 +13,64 @@ jest.mock('express-oauth2-jwt-bearer', () => ({
   })
 }))
 
+const userOneID = new Types.ObjectId()
+const userTwoID = new Types.ObjectId()
+const chatID = new Types.ObjectId()
+const messageOneID = new Types.ObjectId()
+
+// Set up in-memory db before each test.
+beforeAll(async () => {
+  await serverConnect()
+  // TODO:
+  // Create 2 users
+  await User.create(
+    {
+      _id: userOneID,
+      authId: 'a1b2c3',
+      username: 'test user one',
+      chats: [chatID]
+    },
+    {
+      _id: userTwoID,
+      authId: 'd4e5f6',
+      username: 'test user two',
+      chats: [chatID]
+    }
+  )
+  // Create messages in chat
+  await Message.create({
+    _id: messageOneID,
+    sender: userOneID,
+    message: 'This is a test message!',
+    chatId: chatID,
+    timestamp: new Date()
+  })
+  // Create chat
+  await Chat.create({
+    _id: chatID,
+    owner: userOneID,
+    receivers: [userOneID, userTwoID],
+    messages: [messageOneID]
+  })
+})
+
+// Tear down up in-memory db before each test.
+afterAll(async () => {
+  await serverDisconnect()
+})
+
 describe('Chat Routes', () => {
   test('test route', async () => {
     const res = await request(app).get('/api/v1/chats/hello')
 
     expect(res.status).toBe(200)
     expect(res.body.msg).toMatch(/world/i)
+  })
+
+  test('GET /id: should return a chat', async () => {
+    const res = await request(app).get(`/api/v1/chats/${chatID}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveProperty('owner', userOneID.toString())
   })
 })
--- server/src/routes/chatRoutes.ts
+++ server/src/routes/chatRoutes.ts
@@ -1,9 +1,31 @@
-import express, { Express, Request, Response, Router } from 'express'
+import express, { NextFunction, Request, Response, Router } from 'express'
+import { Chat } from '../models'
+import { HydratedDocument } from 'mongoose'
+import { IChat } from '../interfaces'
 
 const router: Router = express.Router()
 
 router.get('/hello', (req: Request, res: Response) => {
   res.status(200).json({ msg: 'world' })
 })
+
+router.get(
+  '/:chatId',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { chatId } = req.params
+      const chat: HydratedDocument<IChat> | null = await Chat.findById(
+        chatId
+      ).populate('messages')
+
+      if (!chat) {
+        return res.status(401).json({ msg: `No chat with id: ${chatId}` })
+      }
+      res.json(chat)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
 
 export default router
